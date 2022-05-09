53579daca4a43ffb11ba67ab784f616c36492caa
MalgorzataIzdebska <m.izdebska1998@gmail.com>
2022-05-09 20:52:47 +0200

    Newton 11

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ t = int( raw_input() )
 for i in range(t):
 	n,k = map( int, raw_input().split() )
 	if k == 0 or k == n : print ("1")
-	else : print silnia(n,k)
+	else : print (silnia(n,k))
 
 #while(t):
 	#n,k=input().split()
