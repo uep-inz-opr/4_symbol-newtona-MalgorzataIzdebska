3461a055877daa92e5ed4afdb10ef9e7bc6da9d5
MalgorzataIzdebska <m.izdebska1998@gmail.com>
2022-05-09 20:51:33 +0200

    Newton 10

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ t = int( raw_input() )
  
 for i in range(t):
 	n,k = map( int, raw_input().split() )
-	if k == 0 or k == n : print '1'
+	if k == 0 or k == n : print ("1")
 	else : print silnia(n,k)
 
 #while(t):
