217e1bf3067f057b48aa54718ae0b3cc4f26b143
MalgorzataIzdebska <m.izdebska1998@gmail.com>
2022-05-09 21:15:46 +0200

    Newton 15

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from math import *
 #from math import factorial
 
 def silnia(n, k):
-	return factorial(6) // (factorial(4) * factorial(2))
+	return factorial(n) // (factorial(k) * factorial(n-k))
+
 
 #t=int(stdin.readline());
 #t = int(input())
@@ -21,7 +22,7 @@ def silnia(n, k):
 
 #while(t):
 	#n,k=input().split()
-	#n=int(6)
-	#k=int(4)
-	#print(silnia(n,k))
+	n=int(6)
+	k=int(4)
+	print(silnia(n,k))
 	
