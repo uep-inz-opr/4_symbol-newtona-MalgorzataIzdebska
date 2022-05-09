90396b0a1a905d2278d1710ab20c9fb9f4c102be
MalgorzataIzdebska <m.izdebska1998@gmail.com>
2022-05-09 20:46:40 +0200

    Newton 8

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from math import factorial
 
 def silnia(n, k):
 	return factorial(n) // (factorial(k) * factorial(n-k))
-t=int(stdin.readline());
-while(t):
-	n,k=input().split()
-	n=int(6)
-	k=int(4)
-	print(silnia(n,k))
+
+#t=int(stdin.readline());
+t = int( raw_input() )
+ 
+for i in range(t):
+n,k = map( int, raw_input().split() )
+if k == 0 or k == n : print '1'
+else : print silnia(n,k)
+
+#while(t):
+	#n,k=input().split()
+	#n=int(6)
+	#k=int(4)
+	#print(silnia(n,k))
 	
