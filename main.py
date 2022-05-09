ff2f999dd9442cf7aaa54441d7ed1e0e3a2071ed
MalgorzataIzdebska <m.izdebska1998@gmail.com>
2022-05-09 21:25:03 +0200

    Newton 18

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ def silnia(n, k):
 	return factorial(n) // (factorial(k) * factorial(n-k))
 
 t = input()
-n,k=input().split()
+n, k = input().split()
  
 for i in range(t):
 	n,k = map(int, input().split())
 	if k == 0 or k == n : print ("1")
-	else : print (silnia(n,k))
+	else : print (silnia(n, k))
 
-	n=int(6)
-	k=int(4)
-	print(silnia(n,k))
+	n = int(6)
+	k = int(4)
+	print (silnia(n, k))
 
 	
