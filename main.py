fc09797f46ca40b08be98f54afd278830338cb09
MalgorzataIzdebska <m.izdebska1998@gmail.com>
2022-05-09 21:21:02 +0200

    Newton 16

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 from math import *
 #factorial(6)/(factorial(4)*factorial(2))
 
-
-
-
-
 #from sys import stdin
 #from math import factorial
 
 def silnia(n, k):
 	return factorial(n) // (factorial(k) * factorial(n-k))
 
-
-#t=int(stdin.readline());
-#t = int(input())
+t = int(input())
+n,k=input().split()
  
-#for i in range(t):
-	#n,k = map(int, input().split())
-	#if k == 0 or k == n : print ("1")
-	#else : print (silnia(n,k))
+for i in range(t):
+	n,k = map(int, input().split())
+	if k == 0 or k == n : print ("1")
+	else : print (silnia(n,k))
 
-#while(t):
-	#n,k=input().split()
 	n=int(6)
 	k=int(4)
 	print(silnia(n,k))
+
 	
